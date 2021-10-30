112d84b19b6e8a7e1689e41ca9f14d414539cb00
Sunwart <li.sa.vectorart@gmail.com>
2021-10-30 18:04:27 +0300

    Task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player(document.querySelector('#vimeo-player'));
+
+player.on('timeupdate', throttle(timeupdate, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(Number.parseFloat(localStorage.getItem('videoplayer-current-time')));
+}
+
+function timeupdate(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    event.seconds === event.duration ? 0 : event.seconds,
+  );
+}
